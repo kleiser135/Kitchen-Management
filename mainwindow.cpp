3396983a5f157c7410e44bca92b45c05038106a2
khang630 <khangnguyen520@yahoo.com>
2021-06-30 13:41:23 -0500

    GUI Main Menu Login
    The basic menu for the restaurant login. Currently has default values for PINs.
    
    Presents an error message if you type in anything other than "cook", "server", or "manager"

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox> //need this in order to use error boxes, the form is QMessageBox::warning(this, "Name of error box", "Message within error box");
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,5 +12,28 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString pin = ui->lineEditPin->text(); //store the string that the user enters in the "enter PIN box"
+
+    if(pin == "cook") //compare PIN with default values
+    {
+        //open cooks menu
+    }
+    else if(pin == "server")
+    {
+        //opens server menu
+    }
+    else if(pin == "manager")
+    {
+        //opens manager menu
+    }
+    else
+    {
+        QMessageBox::warning(this, "Login Error", "Invalid pin, Please enter a valid pin."); //prints error message if the pin is wrong
+    }
 }
 
